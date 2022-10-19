304f9147101aa5184f5da9a0d1550dd2604e51a6
GabrielSBrito17 <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>
2022-10-19 18:31:53 -0300

    api gerenciador de gastos

---
+++ api/schemas/conta_schema.py
@@ -0,0 +1,14 @@
+from api import ma
+from ..models import conta_model
+from marshmallow import fields
+from ..schemas import transacao_schema
+
+class ContaSchema(ma.SQLAlchemyAutoSchema):
+    transacoes = ma.Nested(transacao_schema.TransacaoSchema, many=True)
+    class Meta:
+        model = conta_model.Conta
+        load_instante = True
+
+    nome = fields.String(required=True)
+    descricao = fields.String(required=True)
+    saldo = fields.Float(required=True)
